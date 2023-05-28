9be3e80dddb9b681785a407daf5fb4af34039a3e
Shiv Bhonde <shivbhonde04@gmail.com>
2023-05-28 09:59:29 +0530

    make nvim transparent

--- plugins/community.lua
+++ plugins/community.lua
@@ -10,4 +10,5 @@ return {
   { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim"},
   { import = "astrocommunity.motion.mini-move"},
   { import = "astrocommunity.motion.nvim-surround"},
+  { import = "astrocommunity.utility.transparent-nvim"},
 }
